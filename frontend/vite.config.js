bfc30a282b290c8924238fa8176e09edc6d56d65
m2kky <Muhammedmekky4@gmail.com>
2026-02-15 11:39:14 +0200

    Debugging Mixed Content Errors

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,5 +3,15 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+    plugins: [react()],
+    server: {
+        allowedHosts: ['missed-legend-schemes-governance.trycloudflare.com'],
+        proxy: {
+            '/api': {
+                target: 'http://localhost:3000',
+                changeOrigin: true,
+                secure: false,
+            }
+        }
+    }
 })
